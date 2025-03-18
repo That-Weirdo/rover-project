3819171f513d8ddb4d800f8a499851070f188b64
jpsalis <jpsalis@outlook.com>
2025-03-18 14:11:05 -0400

    Work on adding ros2_control

--- src/rover/setup.py
+++ src/rover/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'rover'
@@ -10,6 +12,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('bringup','launch','*launch.py'))), 
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('bringup', 'config', '*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('description', 'urdf', '*'))),
+        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('description', 'worlds', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
